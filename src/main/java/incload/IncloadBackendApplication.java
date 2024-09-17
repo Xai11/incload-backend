6b83ce157263905cc15581883e6ce072dcef22d3
Xai11 <sashakochetov300@gmail.com>
2024-09-17 19:54:47 +0300

    Initial commit

---
+++ src/main/java/incload/IncloadBackendApplication.java
@@ -0,0 +1,13 @@
+package incload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncloadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncloadBackendApplication.class, args);
+	}
+
+}
