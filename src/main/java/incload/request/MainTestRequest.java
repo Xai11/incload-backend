ff2a746b2db90df24a7a0abbf48a5970606f14bf
Ladislav-Sandalov <v.sandalov2@yandex.ru>
2024-09-23 21:04:48 +0300

    request

---
+++ src/main/java/incload/request/MainTestRequest.java
@@ -0,0 +1,8 @@
+package incload.request;
+
+public class MainTestRequest {
+    public static void main(String[] args){
+        SearhRequest srh = new SearhRequest("vk");
+        srh.Searh();
+    }
+}
